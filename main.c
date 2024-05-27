caf198c27c35b8bd9903a62dd7473f193c406b01
habijey <habilanjeyarasa@gmail.com>
2024-05-27 10:17:44 +0200

    Update main.c

--- main.c
+++ main.c
@@ -13,42 +13,77 @@ int main(){
 
     int bobby_la = 0;
     int nbs = 0;
-    int* pnbs = NULL;
     int nbstot = det_nbs();
     int nbspotentielle = 0;
     int nbcaillou = 0;
     Salle donj[nbstot];
     int matrice[DTAILLE][DTAILLE];
-
+    int a;
+    int choix=-1;
+    int i=0;
     /// partie aug à completer
     Perso perso;
     Perso* pperso = NULL;
     pperso = &perso;
     if(pperso == NULL){
-    	printf("pperso probléme");
-    	exit(2);
+        printf("pperso probléme");
+        exit(2);
+    }
+    Perso tombe;
+    Perso* ptombe = NULL;
+    ptombe = &tombe;
+    if(ptombe == NULL){
+        exit(21);
+        printf("ptombe probléme");
     }
 
-    perso = initialisation_perso();
-    ///
-	creer_s_origine(donj, matrice, &nbs, &nbspotentielle);
-    matrice[perso.xy.x+CST][perso.xy.y+CST] = 1;
 
-    switch(accueil(nomJoueur, &graine)){
-        case 1:  //commencement d'une nouvelle partie
+    do{
+        creer_s_origine(donj, matrice, &nbs, &nbspotentielle);
+        perso = initialisation_perso();
+        matrice[perso.xy.x+CST][perso.xy.y+CST] = 1;
+        a=accueil(nomJoueur, &graine);
+        switch(a){
+            case 1:  //commencement d'une nouvelle partie
+            break;
+
+            case 2:  // récupération de la sauvegarde
+                a=lecture(nomJoueur, &graine, &nbs, &chrono, &nbstot, &bobby_la, &nbcaillou, donj, pperso, matrice, ptombe);
+                if(a==0||a==-1){
+                    a=0;
+                }else{
+                    a=2;
+                }
+            break;
+            case 4:
+                pperso->nb_caillou_depose=4;
+            break;
+            default:
+                return 0;
+            break;
+        }
+        if(a==1||a==2){
             srand(graine);
-            enchainement(pperso,matrice,donj,&nbs,nbstot,&nbspotentielle,&nbcaillou,nbstot,&bobby_la);
-            sauvegarde(nomJoueur, graine, nbs, chrono, nbstot, bobby_la, nbcaillou, donj, pperso, matrice);
-        break;
+            enchainement(pperso,matrice, donj,&nbs,nbstot,&nbspotentielle,&nbcaillou,nbstot,&bobby_la, ptombe);
+            sauvegarde(nomJoueur, graine, nbs, chrono, nbstot, bobby_la, nbcaillou, donj, pperso, matrice, ptombe); 
+        }
+        
+        if(pperso->nb_caillou_depose==4){
 
-        case 2:  // récupération de la sauvegarde
-            lecture(nomJoueur, &graine, &nbs, &chrono, &nbstot, &bobby_la, &nbcaillou, donj, pperso, matrice);
-            enchainement(pperso,matrice,donj,&nbs,nbstot,&nbspotentielle,&nbcaillou,nbstot,&bobby_la);
-        break;
-        default:
-            return 0;
-        break;
-    }
+            do{
+                system("clear");
+                printf("\n1: revenir au menu   2: sortir du jeu \n");
+                if(i==1){
+                    printf("Erreur de saisie\n");
+                    vide_buffer();
+                }
+                i=1;
+            }while(scanf("%d", &choix)!=1||choix!=1 && choix!=2);
+            if(choix==2){
+                return 0;
+            }
+        }
 
 
+    }while(a!=3);
 }
